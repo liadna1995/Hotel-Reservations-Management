b145be18fb08e6c103d812482cd3fa57ec24df11
liadna1995 <liadna1995@gmail.com>
2022-02-16 14:47:58 +0200

    comm

--- force-app/main/default/lwc/hotelsMapComponent/hotelsMapComponent.js
+++ force-app/main/default/lwc/hotelsMapComponent/hotelsMapComponent.js
@@ -1,35 +1,38 @@
-import { LightningElement , wire, track} from 'lwc';
+import { LightningElement, wire, track } from "lwc";
 
 import getHotelAddresses from "@salesforce/apex/ApexController.getHotelAddresses";
 
 export default class hotelsMapComponent extends LightningElement {
+  @track error; //this holds errors
 
-    @track error;   //this holds errors
+  @track mapMarkers = [];
+  @track markersTitle = "All Hotels";
+  @track zoomLevel = 4;
+  /* Load address information from Controller */
+  @wire(getHotelAddresses, {})
+  wiredHotelLocations({ error, data }) {
+    if (data) {
+      data.forEach((dataItem) => {
+        this.mapMarkers = [
+          ...this.mapMarkers,
+          {
+            location: {
+              PostalCode: dataItem.PostalCode,
+              Street: dataItem.BillingStreet,
+              City: dataItem.BillingCity,
+              Country: dataItem.BillingCountry,
+              State: dataItem.State
+            },
 
-    @track mapMarkers = [];
-    @track markersTitle = 'All Hotels';
-    @track zoomLevel = 4;
-    /* Load address information from Controller */
-    @wire(getHotelAddresses,{})
-    wiredHotelLocations({ error, data }) {
-        if (data) {            
-            data.forEach(dataItem => {
-                this.mapMarkers = [...this.mapMarkers ,
-                    {
-                        location: {
-                            City: dataItem.BillingCity,
-                            Country: dataItem.BillingCountry,
-                        },
-        
-                        icon: 'standard:address',
-                        title: dataItem.Name,
-                    }                                    
-                ];
-              });            
-            this.error = undefined;
-        } else if (error) {
-            this.error = error;
-        }
+            icon: "standard:address",
+            title: dataItem.Name,
+            description: dataItem.Description
+          }
+        ];
+      });
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
     }
-
+  }
 }
--- force-app/main/default/lwc/searchHotelsComponent/searchHotelsComponent.js
+++ force-app/main/default/lwc/searchHotelsComponent/searchHotelsComponent.js
@@ -2,31 +2,43 @@ import { LightningElement, wire, track } from "lwc";
 import searchHotel from "@salesforce/apex/ApexController.searchHotel";
 
 export default class SearchHotelsComponent extends LightningElement {
-
-  @track hotelName = '';
+  @track hotelName = "";
   @track hotels_list = [];
   @track error;
-  @track searchTitle = 'Search For Hotels';
-  
-  
+  @track searchTitle = "Search For Hotels";
 
-  @wire(searchHotel, { hotelNameArg: '$hotelName' })
+  @wire(searchHotel, { hotelNameArg: "$hotelName" })
   dataTableAcc({ error, data }) {
-    if (data) { 
-      this.hotels_list = data;
+    if (data) {
+      let rows = [];
+      let tempRows = JSON.parse(JSON.stringify(data));
+      // Update json arr before presenting
+      for (let i = 0; i < tempRows.length; i++) {
+        let row = tempRows[i];
+        // Update profile link for the href in the HTML
+        row.profile_link =
+          "https://ass-liad-dev-ed.lightning.force.com/lightning/r/Account/" +
+          row.Id +
+          "/view";
+        console.log(row);
+        if (row.Picture__c === undefined) {
+          // No image available picture
+          row.Picture__c =
+            "https://www.segen.co.uk/wp-content/uploads/2020/12/SE-25000-R4-APP.jpg";
+        }
+        rows.push(row);
+      }
+
+      this.hotels_list = rows;
       this.error = undefined;
     } else if (error) {
-        this.error = error;
+      this.error = error;
     }
   }
-  
+
   // Update new hotel search input
   handleSearchInputChange(event) {
     const searchString = event.target.value;
     this.hotelName = searchString;
   }
-
-
-
-  
 }
